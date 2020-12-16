6eb57c3c74deb5895eff92f3c1ec83b7982dd17b
Nadezhda <catsnake@yandex.ru>
2020-12-16 20:13:23 +0300

    add fetch

--- js/script.js
+++ js/script.js
@@ -233,13 +233,7 @@ window.addEventListener("DOMContentLoaded", () => {
       `;
       form.insertAdjacentElement("afterend", statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      );
+      // request.setRequestHeader();
 
       const formData = new FormData(form);
 
@@ -248,23 +242,36 @@ window.addEventListener("DOMContentLoaded", () => {
         object[key] = value;
       });
 
-      const json = JSON.stringify(object);
+      fetch("server.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(object),
+      })
+        .then((data) => data.text())
+        .then((data) => {
+          console.log(data);
+          showThanksModal(message.success);
+          statusMessage.remove();
+        })
+        .catch(() => {
+          showThanksModal(message.failure);
+        })
+        .finally(() => {
+          form.reset();
+        });
 
-      request.send(json);
-
-      request.addEventListener("load", () => {
+      /*  request.addEventListener("load", () => {
         if (request.status === 200) {
           console.log(request.response);
           showThanksModal(message.success);
           form.reset();
           statusMessage.remove();
-          /* setTimeout(() => {
-            statusMessage.remove();
-          }, 2000); */
         } else {
           showThanksModal(message.failure);
         }
-      });
+      }); */
     });
   }
 

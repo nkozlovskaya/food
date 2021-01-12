fadb5ba06d99bc7adff33e4c2c9094e509ad4355
Nadezhda <catsnake@yandex.ru>
2021-01-12 17:52:36 +0300

    modified slider

--- js/script.js
+++ js/script.js
@@ -361,8 +361,17 @@ window.addEventListener("DOMContentLoaded", () => {
   let slideIndex = 1;
   let offset = 0;
 
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
+
   slidesField.style.width = 100 * slides.length + '%';
-  slidesField.style.dysplay = 'flex';
+  slidesField.style.display = 'flex';
   slidesField.style.transition = '0.5s all';
 
   slidesWrapper.style.overflow = 'hidden';
@@ -373,13 +382,27 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
       offset = 0;
     } else {
       offset += +width.slice(0, width.length - 2);
     }
 
-    slidesField.style.transform = `translateX(-${offset})px`;
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
   });
 
   prev.addEventListener('click', () => {
@@ -389,7 +412,20 @@ window.addEventListener("DOMContentLoaded", () => {
       offset -= +width.slice(0, width.length - 2);
     }
 
-    slidesField.style.transform = `translateX(-${offset})px`;
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
   });
 
 
